5d9f4da10611fd7fdc1dc9f9023ef18ac9f74106
Shankaran <skshankaran4@gmail.com>
2024-02-27 23:03:53 +0530

    first commit

---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      <header>TSK Ecommerce</header>
+      <main></main>
+      <footer>All Rights Reserved</footer>
+    </div>
+  )
+}
+
+export default App
