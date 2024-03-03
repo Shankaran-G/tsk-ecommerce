c217902ff330e4b990188e9e5a240c0686d6435d
Shankaran <skshankaran4@gmail.com>
2024-03-03 17:59:53 +0530

    Second Commit

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,44 @@
-import './App.css'
+import { Container, Navbar, Nav, Row, Col } from 'react-bootstrap'
+import { smapleProducts } from './data'
 
 function App() {
   return (
-    <div>
-      <header>TSK Ecommerce</header>
-      <main></main>
-      <footer>All Rights Reserved</footer>
+    <div className="d-flex flex-column vh-100">
+      <header>
+        <Navbar bg="dark" variant="dark" expand="lg">
+          <Container>
+            <Navbar.Brand>TSK Ecommerce</Navbar.Brand>
+          </Container>
+          <Nav>
+            <a href="/cart" className="nav-link">
+              Cart
+            </a>
+            <a href="/signin" className="nav-link">
+              Sign In
+            </a>
+          </Nav>
+        </Navbar>
+      </header>
+      <main>
+        <Container className="mt-3">
+          <Row>
+            {smapleProducts.map((product) => (
+              <Col key={product.slug} sm={6} md={4} lg={3}>
+                <img
+                  src={product.image}
+                  alt={product.name}
+                  className="product-image"
+                />
+                <h2>{product.name}</h2>
+                <p>Rs.{product.price}</p>
+              </Col>
+            ))}
+          </Row>
+        </Container>
+      </main>
+      <footer>
+        <div className="text-center">All Righrs Approved</div>
+      </footer>
     </div>
   )
 }
